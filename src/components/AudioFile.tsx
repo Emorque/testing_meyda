516d09d87eab95a7eb0b13c4a18c74eb931bdb85
Eric Rodriguez <emorque10@gmail.com>
2025-01-22 21:55:37 -0500

    Experimenting with a meter

--- src/components/AudioFile.tsx
+++ src/components/AudioFile.tsx
@@ -4,6 +4,7 @@ import Meyda, { MeydaFeaturesObject } from 'meyda';
 import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import "./audiofile.css";
 import gsap from 'gsap';
+import { relative } from 'path';
 
 export function AudioFile() {    
     const [audioURL, setAudioURL] = useState<string | null>(null)
@@ -12,11 +13,15 @@ export function AudioFile() {
     const [stageBtn, setStageBtn] = useState<boolean>(false); 
 
     const gameContainer = useRef<HTMLDivElement>(null);
+    // const gameContainerHolds = useRef<HTMLDivElement>(null);
     const gameWrapper = useRef<HTMLDivElement>(null);
     const aCircle = useRef<HTMLDivElement>(null);
     const dCircle = useRef<HTMLDivElement>(null);
+    const sCircle = useRef<HTMLDivElement>(null);
+    const barRef = useRef<HTMLDivElement>(null);
+    const barContainerRef = useRef<HTMLDivElement>(null);
 
-    const [rotated, setRotate] = useState<boolean>(false);
+    const [rotated, setRotate] = useState<boolean>(true);
     const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
     // stopwatch
@@ -24,35 +29,15 @@ export function AudioFile() {
     const [stPaused, setStPaused] = useState<boolean>(true);
     const [time, setTime] = useState<number>(0);
 
-    useEffect(() => {
-
-      if (stopwatchActive && !stPaused) {
-        intervalRef.current = setInterval(() => {
-          setTime((time) => time + 10);
-          console.log(time);
-
-        }, 10)}
-      else {
-        if (intervalRef.current) {
-          clearInterval(intervalRef.current);
-          intervalRef.current = null
-        };
-      }
-      return () => {
-        if (intervalRef.current) {
-          clearInterval(intervalRef.current);
-          intervalRef.current = null
-        };
-      }
-      }, [stopwatchActive, stPaused]);
-
     const [aList, setAList] = useState<number[]>([]);
     const [dList, setDList] = useState<number[]>([]);
+    const [sList, setSList] = useState<number[]>([]);
 
     const [amplitudeSpectrum, setAmplitudeSpectrum] = useState<Float32Array<ArrayBufferLike>>(new Float32Array(0));
 
     const [aActive, setAActive] = useState<boolean>(false);
     const [dActive, setDActive] = useState<boolean>(false);
+    const [sActive, setSActive] = useState<boolean>(false);
 
     const [score, setScore] = useState<number>(0);
     const [hitCount, setHitCount] = useState<number>(0);
@@ -66,14 +51,24 @@ export function AudioFile() {
         const file = e.target.files?.[0];
         if (!file) return;
         setAudioURL(URL.createObjectURL(file));
+
         setMusicSet(true);
         setStageBtn(false);
+
         setScore(0);
         setHitCount(0);
         setMissCount(0);
         setNoteCount(0);
+
         setAList([]);
         setDList([]);
+
+        setStopwatchActive(false);
+        setStPaused(true);
+        setTime(0);
+        
+        document.querySelectorAll(".curveHold").forEach(e => e.remove());
+        document.querySelectorAll(".curve").forEach(e => e.remove());
         updateContext();
     }
 
@@ -109,16 +104,51 @@ export function AudioFile() {
         }
     }
 
+    useEffect(() => {
+
+      if (stopwatchActive && !stPaused) {
+        intervalRef.current = setInterval(() => {
+          setTime((time) => time + 10);
+          console.log(time);
+
+        }, 10)}
+      else {
+        if (intervalRef.current) {
+          clearInterval(intervalRef.current);
+          intervalRef.current = null
+        };
+      }
+      return () => {
+        if (intervalRef.current) {
+          clearInterval(intervalRef.current);
+          intervalRef.current = null
+        };
+      }
+      }, [stopwatchActive, stPaused]);
+
+    // const getAmplitudeRangeForKey = (key: string): number[] => {
+    //   switch (key) {
+    //     case 'a':
+    //       return [0, 1, 2, 3, 4, 5];
+    //     case 'd':
+    //       return [6, 7, 8, 9, 10, 11];
+    //     case 'j':
+    //       return [12, 13, 14, 15, 16, 17];
+    //     case 'l':
+    //       return [18, 19, 20, 21, 22, 23];
+    //     default:
+    //       return [];
+    //   }
+    // };
+    
     const getAmplitudeRangeForKey = (key: string): number[] => {
       switch (key) {
         case 'a':
-          return [0, 1, 2, 3, 4, 5];
+          return [0, 1, 2, 3, 4, 5, 6, 7];
         case 'd':
-          return [6, 7, 8, 9, 10, 11];
-        case 'j':
-          return [12, 13, 14, 15, 16, 17];
-        case 'l':
-          return [18, 19, 20, 21, 22, 23];
+          return [8, 9, 10, 11, 12, 13, 14, 15];
+        case 's':
+          return [16, 17, 18, 19, 20, 21, 22, 23];
         default:
           return [];
       }
@@ -133,70 +163,123 @@ export function AudioFile() {
       const buttonStates = [
         { key: 'a', state: aActive, setState: setAActive, circle: aCircle, circleClass: "aCurve", setList : setAList, oppositeList: dList },
         { key: 'd', state: dActive, setState: setDActive, circle: dCircle, circleClass: "dCurve", setList : setDList, oppositeList: aList },
-        { key: 'j', state: aActive, setState: setAActive, circle: aCircle, circleClass: "aCurve", setList : setAList, oppositeList: dList },
-        { key: 'l', state: dActive, setState: setDActive, circle: dCircle, circleClass: "dCurve", setList : setDList, oppositeList: aList },
+        { key: 's', state: sActive, setState: setSActive, circle: sCircle, circleClass: "wCurve", setList : setSList, oppositeList: aList },
+        
+        // { key: 'j', state: aActive, setState: setAActive, circle: aCircle, circleClass: "aCurve", setList : setAList, oppositeList: dList },
+        // { key: 'l', state: dActive, setState: setDActive, circle: dCircle, circleClass: "dCurve", setList : setDList, oppositeList: aList },
       ];
 
       // buttonStates.forEach(({ key, state, setState, pillar, pillarClass, setList }) => {
       buttonStates.forEach(({ key, state, setState, circle, circleClass, setList, oppositeList }) => { 
       // If the button state is already set, skip the update
         if (state) return;
-        if (oppositeList.length > 0 && (oppositeList[-1] + 0.30 >= ((time + 2000)/1000))) {
-          return;
-        }
+        // if (oppositeList.length > 0 && (oppositeList[-1] + 0.30 >= ((time + 2000)/1000))) {
+        //   return;
+        // }
         const spectrumRange = getAmplitudeRangeForKey(key);
         const avgAmplitude = spectrumRange.reduce((sum, idx) => sum + amplitudeSpectrum[idx], 0) / spectrumRange.length;
             
+        console.log(key, avgAmplitude)
         if (avgAmplitude > 1.5) {
-          if (key === 'a' || key === 'j') {
-            if (!aSetThisFrame) {
-              setState(true);
-              setNoteCount(count => count + 1);
-              setList(prevList => [...prevList, ((time + 2000) / 1000)]);
-              aSetThisFrame = true; // Mark that 'a' or 'j' was activated in this frame
-            }
-            else {
-              return
-            }
-          } 
-          else if (key === 'd' || key === 'l') {
-            if (!dSetThisFrame) {
-              setNoteCount(count => count + 1);
-              setState(true);
-              setList(prevList => [...prevList, ((time + 2000) / 1000)]);
-              dSetThisFrame = true; // Mark that 'd' or 'l' was activated in this frame
-            }
-            else {
-              return
-            }
-          }
+          setState(true)
+          setNoteCount(count => count + 1);
+          setList(prevList => [...prevList, ((time + 2000) / 1000)]);
+          // if (key === 'a' || key === 'j') {
+          //   if (!aSetThisFrame) {
+          //     setState(true);
+          //     setNoteCount(count => count + 1);
+          //     setList(prevList => [...prevList, ((time + 2000) / 1000)]);
+          //     aSetThisFrame = true; // Mark that 'a' or 'j' was activated in this frame
+          //   }
+          //   else {
+          //     return
+          //   }
+          // } 
+          // else if (key === 'd' || key === 'l') {
+          //   if (!dSetThisFrame) {
+          //     setNoteCount(count => count + 1);
+          //     setState(true);
+          //     setList(prevList => [...prevList, ((time + 2000) / 1000)]);
+          //     dSetThisFrame = true; // Mark that 'd' or 'l' was activated in this frame
+          //   }
+          //   else {
+          //     return
+          //   }
+          // }
           // else if (key === 'd' || key === 'l') return;
 
           if (circle.current) {
-            const newEle = document.createElement('p');
-            newEle.classList.add(circleClass)
-            newEle.classList.add("curve")
-            newEle.textContent= ""
-            circle.current.appendChild(newEle);
-            newEle.addEventListener("animationend", () => {
-              circle.current?.removeChild(newEle);
-            })
-          }
-          // Handle the timeout for each button independently
-          if (key === 'a' || key === 'j') {
-            setTimeout(() => {
-              setState(false);
-            }, 1000);
-          }
-          else if (key === 'd' || key === 'l') {
-            setTimeout(() => {
-              setState(false);
-            }, 750);
-          }
+            
+            // newEle.classList.add("curve")
+            if (circle === sCircle) {
+              // newEle.classList.add("wholeCurve")
+              setTimeout(() => {
+                if (barContainerRef.current) {
+                  console.log("JHIIJIJIJI")
+                  barContainerRef.current.style.border = '10px solid green';
+                }
+              }, 500)
+              setTimeout(() => {
+                if (barContainerRef.current) {
+                  barContainerRef.current.style.border = '10px solid rgb(250, 238, 223)';
+                }
+              }, 700)
 
+              setTimeout(() => {
+                if (barContainerRef.current) {
+                  console.log("JHIIJIJIJI")
+                  barContainerRef.current.style.border = '10px solid green';
+                }
+              }, 1000)
+              setTimeout(() => {
+                if (barContainerRef.current) {
+                  barContainerRef.current.style.border = '10px solid rgb(250, 238, 223)';
+                }
+              }, 1200)
+
+              setTimeout(() => {
+                if (barRef.current) {
+                  barRef.current.style.height = '100px';
+                }
+              }, 1500)
+
+              setTimeout(() => {
+                if (barRef.current) {
+                  barRef.current.style.height = '200px';
+                }
+              }, 2000)
+
+              setTimeout(() => {
+                if (barRef.current) {
+                  barRef.current.style.height = '0px';
+                }
+              }, 2500)
+              // if (barRef.current) barRef.current.style.height = "50px"
+            }
+            else {
+              const newEle = document.createElement('p');
+              newEle.classList.add(circleClass)
+              newEle.classList.add("curve")
+              newEle.textContent= ""
+              circle.current.appendChild(newEle);
+              newEle.addEventListener("animationend", () => {
+                circle.current?.removeChild(newEle);
+              })
+            }
+          }
+          if (key === 'a' || key === 'd') {
+            setTimeout(() => {
+              setState(false)
+            }, 500)  
+          }
+          else {
+            setTimeout(() => {
+              setState(false)
+            }, 2500)
+          }
         }
       });
-    }, [amplitudeSpectrum, aActive, dActive, time, dList, aList]);
+    }, [amplitudeSpectrum, aActive, dActive, time, dList, aList, sList, sActive]);
 
     useEffect(() => {
       // console.log("AList", aList);
@@ -234,6 +317,24 @@ export function AudioFile() {
       }
     }, [dList, time])
 
+    useEffect(() => {
+      // console.log("DList", dList);
+      if (sList.length > 0 && sList[0] + 0.40 < time/1000) {
+        setScore(score => score - 1);
+        setMissCount(count => count + 1);
+        setSList(sList => sList.slice(1));
+
+        const message = document.createElement('p');
+        message.classList.add("message");
+        message.classList.add("sMessage");
+        message.textContent= "missed";
+        if (gameWrapper.current) gameWrapper.current.appendChild(message);
+        setTimeout(() => {
+          if (gameWrapper.current) gameWrapper.current.removeChild(message);  
+        }, 500);
+      }
+    }, [sList, time])
+
 
     useEffect(() => {
       const handleKeyDown = (event: { key: string; }) => {
@@ -261,8 +362,6 @@ export function AudioFile() {
                 setScore(score => score + 5);
                 setHitCount(count => count + 1);
                 setDList(dList => dList.slice(1));
-                console.log("ex3", dList, (time/1000))
-                console.log("hit")
                 // message.classList.add("success");
                 // message.classList.add("successD");
                 message.textContent= "perfect";
@@ -355,8 +454,57 @@ export function AudioFile() {
           }
         }
 
-        if (event.key === 'd' || event.key === 'D' ) {
+        if (event.key === 'k' || event.key === 'K' ) {
           setDBtn(true);
+          const message = document.createElement('p');
+          message.classList.add("message");
+          // message.classList.add("sMessage");
+          if (sList.length === 0) {
+            // message.textContent= "early";
+            // if (gameContainerHolds.current) gameContainerHolds.current.appendChild(message);
+            // setTimeout(() => {
+            //   if (gameContainerHolds.current) gameContainerHolds.current.removeChild(message);  
+            // }, 500);
+          }
+          else {
+            if ((time/1000) < sList[0] - 0.40) {  
+              message.textContent= "early";
+              message.classList.add("sMessageEarly");
+              if (gameWrapper.current) gameWrapper.current.appendChild(message);
+              setTimeout(() => {
+                if (gameWrapper.current) gameWrapper.current.removeChild(message);  
+              }, 500);
+            }
+            else if (sList[0] + 0.25 >= (time/1000) && (time/1000) > sList[0] - 0.25) {
+              setScore(score => score + 5);
+              setHitCount(count => count + 1);
+              setSList(sList => sList.slice(1));
+
+              message.classList.add("sMessage");
+              message.textContent= "perfect";
+              message.style.backgroundColor = "green";
+              if (gameWrapper.current) gameWrapper.current.appendChild(message);
+              setTimeout(() => {
+                if (gameWrapper.current) gameWrapper.current.removeChild(message);  
+              }, 500);
+            }
+            else if (sList[0] + 0.40 >= (time/1000) && (time/1000) > sList[0] - 0.40) {
+              setScore(score => score + 3);
+              setHitCount(count => count + 1);
+              setSList(sList => sList.slice(1));
+              message.classList.add("sMessage");
+              message.textContent= "success";
+              message.style.backgroundColor = "green";
+              if (gameWrapper.current) gameWrapper.current.appendChild(message);
+              setTimeout(() => {
+                if (gameWrapper.current) gameWrapper.current.removeChild(message);  
+              }, 500);
+            }
+          }
+        }
+
+        if (event.key === 's' || event.key === 'S' ) {
+          setSBtn(true);
 
           if (dList.length === 0 || aList.length === 0) {
             setScore(score => score - 10);
@@ -430,6 +578,9 @@ export function AudioFile() {
         if (event.key === 'k' || event.key === 'K') {
           setKBtn(false);
         }
+        if (event.key === 's' || event.key === 'S') {
+          setSBtn(false);
+        }
       }
       document.addEventListener('keyup', handleKeyUp);
   
@@ -442,11 +593,11 @@ export function AudioFile() {
     const handleFlip = () => {
       console.log("test");
       if (rotated) {
-        gsap.to("#gamecontainer-circle", {rotate: "60deg"})
+        gsap.to("#gamecontainer-circle", {rotate: "200deg"})
         setRotate(false);
       } 
       else {
-        gsap.to("#gamecontainer-circle", {rotate: "120deg"})
+        gsap.to("#gamecontainer-circle", {rotate: "160deg"})
         setRotate(true);
       }
     }
@@ -460,14 +611,26 @@ export function AudioFile() {
       opacity: rotated? "1" : "0.3",
       transition: 'opacity 0.2s linear'
     }
+    
+    const sStyle = {
+      // opacity: rotated? "1" : "1",
+      // transition: 'opacity 0.2s linear'
+    }
 
     const [aBtn, setABtn] = useState<boolean>(false);
     const [dBtn, setDBtn] = useState<boolean>(false);
     const [kBtn, setKBtn] = useState<boolean>(false); 
+    const [sBtn, setSBtn] = useState<boolean>(false); 
     
     const btnStyleA = {
       backgroundColor: aBtn? "grey" : "black",
       color: aBtn? "black" : "white",
+      padding: 5,
+    }
+
+    const btnStyleS = {
+      backgroundColor: sBtn? "grey" : "black",
+      color: sBtn? "black" : "white",
       padding: 5,
     }
     const btnStyleD = {
@@ -539,19 +702,34 @@ export function AudioFile() {
             </div>
             <input type="file" accept='audio/*' onChange={audioChange}/>
 
+            <p>{time}</p>
+
             <audio src={audioURL ?? ""} controls={false} ref={audioRefListening} loop={false} />
             <audio src={audioURL ?? ""} controls={false} ref={audioRefSetting} loop={false} />
             {musicSet && <button onClick={setMusicStage} disabled={stageBtn}>Set Stage</button>}
             <div style={activeCircle}> Active </div>
 
             <div ref={gameWrapper} style={{position: "relative"}}>
-              <div id='gamecontainer-circle' ref={gameContainer}>
-                <div className='click-Area caA' style={aStyle} ref={aCircle}></div>
-                <div className='click-Area caD' style={dStyle} ref={dCircle}></div>
+              <div style={{display: 'flex', gap: 20, alignItems: 'flex-end', flexDirection: 'row'}}>
+                <div id='gamecontainer-circle' ref={gameContainer}>
+                  <div className='caWhole' ref={sCircle}></div>
+                  <div className='click-Area caA' style={aStyle} ref={aCircle}></div>
+                  <div className='click-Area caD' style={dStyle} ref={dCircle}></div>
+                  {/* <div className='click-Area caS' style={sStyle} ref={sCircle}></div> */}
+                </div>
+                <div className='bar-container' ref={barContainerRef}>
+                  <div className='bar' ref={barRef}></div>
+                </div>
 
+
+                {/* <div id='gamecontainer-holds' ref={gameContainerHolds}>
+                  <div className='click-AreaHold' style={sStyle} ref={sCircle}></div>
+                </div> */}
               </div>
+              
               <div id='button-container'>
                 <div style={btnStyleA}>A</div>
+                <div style={btnStyleS}>S</div>
                 <div style={btnStyleD}>D</div>
                 <div style={btnStyleK}>K</div>
               </div>
